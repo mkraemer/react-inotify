efba1760077ca1fe91f6f1478e1e9f2ddc1dfef3
Marius Krämer <marius.kraemer@mercoline.de>
2013-02-01 17:08:16 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# React-Inotify
+
+Basic inotify bindings for [React PHP](https://github.com/reactphp).
+
+##Install
+This library requires PHP5.4 and the [inotify PECL extension](http://pecl.php.net/package/inotify).
+The best way to install this library is through [composer](http://getcomposer.org):
+
+```JSON
+{
+    "require": {
+        "mkraemer/react-inotify": "dev-master"
+    }
+}
+```
+## Usage
+
+This library provides the Inotify class which takes an event loop and optionally the timer interval in which the inotify events should be checked as constructor arguments.
+After initializing the class, you can use the add() method to add paths to the list of watched paths. Use the [inotify bitmasks](http://www.php.net/manual/en/inotify.constants.php) to define to which filesystem operations to listen.
+
+```php
+<?php
+
+$loop = React\EventLoop\Factory::create();
+$inotify = new MKraemer\ReactInotify\Inotify($loop);
+
+$inotify->add('/tmp/', IN_CLOSE_WRITE + IN_CREATE + IN_DELETE);
+$inotify->add('/var/log/', IN_CLOSE_WRITE + IN_CREATE + IN_DELETE);
+
+$inotify->on(IN_CLOSE_WRITE, function ($path) {
+    echo 'File closed after writing: '.$path.PHP_EOL;
+});
+
+$inotify->on(IN_CREATE, function ($path) {
+    echo 'File created: '.$path.PHP_EOL;
+});
+
+$inotify->on(IN_DELETE, function ($path) {
+    echo 'File deleted: '.$path.PHP_EOL;
+});
+
+$loop->run();
+
+```
---
+++ src/MKraemer/ReactInotify/Inotify.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace MKraemer\ReactInotify;
+
+use Evenement\EventEmitter;
+use React\EventLoop\LoopInterface;
+
+/**
+ * Inotify allows to listen to inotify events emit evenement events
+ */
+class Inotify extends EventEmitter
+{
+    /**
+     * @var resource
+     */
+    protected $inotifyHandler;
+
+    /**
+     * @var array
+     */
+    protected $watchDescriptors;
+
+    /**
+     * Constructor. Initializes the inotifyHandler and
+     * registers a periodicTimer in the eventLoop
+     *
+     * @param React\EventLoop\LoopInterface $loop     Event Loop
+     * @param float                         $interval Interval in which new events should be read
+     */
+    public function __construct(LoopInterface $loop, $interval = 0.1)
+    {
+        $this->inotifyHandler = \inotify_init();
+        stream_set_blocking($this->inotifyHandler, 0);
+
+        $this->watchDescriptors = [];
+
+        $loop->addPeriodicTimer($interval, $this);
+    }
+
+    /**
+     * Checks if new inotify events are available
+     * and emits them via evenement
+     */
+    public function __invoke()
+    {
+        if (false !== ($events = \inotify_read($this->inotifyHandler))) {
+            foreach ($events as $event) {
+                $path = $this->watchDescriptors[$event['wd']]['path'];
+                $this->emit($event['mask'], [$path.$event['name']]);
+            }
+        }
+    }
+
+    /**
+     * Adds a path to the list of watched paths
+     *
+     * @param string  $path      Path to the watched file or directory
+     * @param integer $mask      Bitmask of inotify constants
+     */
+    public function add($path, $mask)
+    {
+        $descriptor = \inotify_add_watch($this->inotifyHandler, $path, $mask);
+        $this->watchDescriptors[$descriptor] = ['path' => $path];
+    }
+}
